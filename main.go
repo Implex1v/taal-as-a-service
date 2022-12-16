90f6a08471cb040b4eab93b9bfb2d8c12490ddba
Sönke Tölke <soenke.toelke@consileo.net>
2022-12-16 12:52:22 +0100

    Enable prometheus metrics

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
 	e.Use(middleware.Recover())
+	enablePrometheus(e)
+
 	e.GET("/words", GetWords)
 	e.GET("/health", GetHealth)
 
@@ -15,4 +19,9 @@ func main() {
 	if err != nil {
 		return
 	}
+}
+
+func enablePrometheus(e *echo.Echo) {
+	p := prometheus.NewPrometheus("echo", nil)
+	p.Use(e)
 }
